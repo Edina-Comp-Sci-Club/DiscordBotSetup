0f8a6349afe042528a94e56655aacc1ec8ca7a7d
Tag <nishaloutagvi@MacBook-Pro.local>
2022-11-09 17:53:13 -0600

    reverted weird stuff

--- DiscordBot/src/main/java/org/example/Event/InteractionEventReceiver.java
+++ DiscordBot/src/main/java/org/example/Event/InteractionEventReceiver.java
@@ -8,17 +8,11 @@ import org.jetbrains.annotations.NotNull;
 public class InteractionEventReceiver extends ListenerAdapter {
 
     @Override
-    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event){
+    public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         super.onSlashCommandInteraction(event);
-
         System.out.println("Slash Commands");
-        switch(event.getName()) {
-            case "hi":
-                event.reply("HI!!!").setEphemeral(true).queue();
-            case "airstrike":
-                event.reply("Predator missiles incoming").setEphemeral(true).queue();
-                event.getChannel().sendMessage("MIDDLE EASTERN CHILDREN RUN IN FEAR").queue();
+        if (event.getName().equals("hi")) {
+            event.reply("HI!!!").setEphemeral(true).queue();
         }
-
     }
 }
--- DiscordBot/src/main/java/org/example/Event/MessageEventListener.java
+++ DiscordBot/src/main/java/org/example/Event/MessageEventListener.java
@@ -16,23 +16,6 @@ public class MessageEventListener  extends ListenerAdapter  {
 
         message = event.getMessage().getContentRaw();
 
-        if(message.equalsIgnoreCase("obama airstrike")){
-            event.getChannel().sendMessage("Predator missiles incoming.").queue();
-            try{TimeUnit.SECONDS.sleep(4);} //this is only in a try/catch bc sleep needs it
-            catch (InterruptedException ex) {}
-            event.getChannel().sendMessage("MIDDLE EASTERN CHILDREN RUN IN FEAR!").queue();
-        }
-
-        if(     message.equalsIgnoreCase("biden 2024") ||
-                message.equalsIgnoreCase("trump 2024") ||
-                message.equalsIgnoreCase("kanye 2024")){
-            event.getChannel().sendMessage("Brent Peterson 2024").queue();
-        }
-
-        int nR = (int)(Math.random() * 10000);
-        if(nR == 50)
-            event.getMessage().reply("https://pbs.twimg.com/media/FdMVn_jakAEJca2?format=jpg&name=360x360").queue();
-
 //        user = event.getMember().getEffectiveName();
 //
 //        System.out.println(user + "said: " + message);
