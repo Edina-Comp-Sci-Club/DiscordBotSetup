69258639e3d17f4d2b8ddcb046820559c05a951d
tagciccone <tagciccone@gmail.com>
2023-03-28 10:24:02 -0500

    initial setup and openai dependency added

--- DiscordBot/src/main/java/org/example/Main.java
+++ DiscordBot/src/main/java/org/example/Main.java
@@ -15,29 +15,25 @@ import javax.security.auth.login.LoginException;
 public class Main {
     public static void main(String[] args) throws InterruptedException, LoginException {
         //This is the Variable that we used to store the token.
-//        final String  TOKEN = "Token Here";
+        final String  TOKEN = "Token Here";
 
-        //The Code below is how we give birth to the bot, just like birth it's a little messy.
-//            JDA bot = JDABuilder.createDefault(TOKEN)//this part connects our code to the discord bot
+            JDA bot = JDABuilder.createDefault(TOKEN)//this part connects our code to the discord bot
 
-//            .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES) // we tell discord with this code that we are trying to retrieve messages.
-//
-//            .addEventListeners(
-//                        new ReadyEventListeners(),
-//                        new MessageEventListener(),
-//                        new InteractionEventReceiver()) // the Listeners here are the ones that wait for a message and lets us know what the message is  we can do what ever we want
+            .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGES) // we tell discord with this code that we are trying to retrieve messages.
+
+            .addEventListeners(
+                        new ReadyEventListeners(),
+                        new MessageEventListener(),
+                        new InteractionEventReceiver()) // the Listeners here are the ones that wait for a message and lets us know what the message is  we can do what ever we want
 
                 //This will indicate whose code is running
-//             .setActivity(Activity.playing("{Insert Name}'s code}"))
+             .setActivity(Activity.playing("beep boop gpt code"))
 
-//             .build(); // Then once we have all that done we build the bot, and give birth to something we will hate or die in the next 5 minutes
+             .build(); // Then once we have all that done we build the bot, and give birth to something we will hate or die in the next 5 minutes
 
         //this is for slash commands
-//        bot.upsertCommand("hi", "I will say hi in the most aggressive way").setGuildOnly(true).queue();
+        bot.upsertCommand("hi", "I will say hi in the most aggressive way").setGuildOnly(true).queue();
 
-        //now try to follow along with code
-
-        //CODE HERE
 
 
 
