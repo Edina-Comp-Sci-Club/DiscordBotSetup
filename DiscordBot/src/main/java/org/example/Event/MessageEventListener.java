f6e9758d5fcacfbbd05e7524cb2d08d5a09a230d
tagciccone <tagciccone@gmail.com>
2023-04-06 11:07:48 -0500

    Proper PNG conversion, rescaling, support for all resolutions

--- DiscordBot/src/main/java/org/example/Event/MessageEventListener.java
+++ DiscordBot/src/main/java/org/example/Event/MessageEventListener.java
@@ -13,17 +13,24 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.example.SessionConstants;
+import org.imgscalr.Scalr;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+
+import javax.imageio.ImageIO;
 
 
 public class MessageEventListener  extends ListenerAdapter {
@@ -68,6 +75,7 @@ public class MessageEventListener  extends ListenerAdapter {
                               **GPT complete**: Completes text entered after *complete*
                               **GPT channel set**: Sets the current channel to be used for interactive chatting. The only command that works in this channel is **GPT prompt**
                               **GPT prompt**: Sets the chatbot's "personality"
+                              **GPT edit**: Provides a variation on an inputted image; Transparent areas become black
                               
                             Admin Commands:
                               **crash**: Shuts down the bot
@@ -93,15 +101,28 @@ public class MessageEventListener  extends ListenerAdapter {
                 }
 
                 if (message.startsWith("GPT edit")) {
-                    URL imageUrl;
-                    String tempDir = System.getProperty("java.io.tmpdir");
-                    String path = tempDir + "botImages/" + LocalTime.now() + ".png";
-                    File imageFile = new File(path);
+                    URL imageUrl; //The URL of the image
+                    String tempDir = System.getProperty("java.io.tmpdir"); //Your computer's temp directory
+                    String path = tempDir + "botImages/" + LocalTime.now() + ".png"; //The filepath as a poorly converted png
+                    File imageFile = new File(path); //The file object
+                    BufferedImage image; //Instantiate bufferedimage
+                    Graphics2D rescaler; //Graphics2D object to resize image
                     System.out.println(path);
                     try {
-                        imageUrl = new URL(event.getMessage().getAttachments().get(0).getUrl());
+                        imageUrl = new URL(event.getMessage().getAttachments().get(0).getUrl()); //Assign image url
                         try {
-                            FileUtils.copyURLToFile(imageUrl, imageFile);
+                            FileUtils.copyURLToFile(imageUrl, imageFile); //Download URL to file in memory
+                            int imageSideLength = Math.max(ImageIO.read(imageFile).getHeight(), ImageIO.read(imageFile).getWidth()); //Get Highest image dimension
+                            int scaledSideLength = (imageSideLength > 768) ? 1024 : ((imageSideLength > 256) ? 512 : 256); //Get what will be the scaled length
+                            image = new BufferedImage( //Image that will be written to
+                                    scaledSideLength,
+                                    scaledSideLength,
+                                    BufferedImage.TYPE_4BYTE_ABGR
+                            );
+                            rescaler = image.createGraphics(); //Declare rescaler
+                            rescaler.drawImage(ImageIO.read(imageFile),0,0, scaledSideLength, scaledSideLength, null); //Draw to output image
+                            rescaler.dispose(); //TODO this doesnt work lol
+                            ImageIO.write(image, "png", imageFile); //Write & properly convert image -> file & PNG
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
@@ -109,16 +130,21 @@ public class MessageEventListener  extends ListenerAdapter {
                         throw new RuntimeException(e);
                     }
 
-                    event.getMessage().reply("Editing image...").queue(message1 -> {messageToEdit = message1;});
-                    CreateImageVariationRequest request = CreateImageVariationRequest.builder()
-                            .responseFormat("url")
-                            .size("512x512")
-                            .build();
-
-                    String generatedImageUrl = service.createImageVariation(request, imageFile).getData().get(0).getUrl();
-                    messageToEdit.editMessage(generatedImageUrl).queue();
-
-
+                    try {
+                        if (Files.size(imageFile.toPath()) > 4000000) {
+                            event.getMessage().reply("Inputted image cannot be more than 4MB when processed, please provide a smaller file.").queue();
+                        } else {
+                            event.getMessage().reply("Editing image...").queue(message1 -> {messageToEdit = message1;});
+                            CreateImageVariationRequest request = CreateImageVariationRequest.builder()
+                                    .responseFormat("url")
+                                    .size("512x512")
+                                    .build();
+                            String generatedImageUrl = service.createImageVariation(request, imageFile).getData().get(0).getUrl();
+                            messageToEdit.editMessage(generatedImageUrl).queue();
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
 
                 if (message.startsWith("GPT complete")) {
